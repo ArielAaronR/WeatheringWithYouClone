18ae5ba7517553e6ec929f691176b0a356b1265e
root <root@ip-172-31-45-134.us-east-2.compute.internal>
2020-03-03 19:11:47 +0000

    added build and server.js fixes

---
+++ build/precache-manifest.8de9701fa8021426c6e966e25233e5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c23bf75eba12d18342c4d6686707599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94101585ba5231333965",
+    "url": "/static/css/main.e59bad2e.chunk.css"
+  },
+  {
+    "revision": "fc95e1dc618864096b5b",
+    "url": "/static/js/2.ade9e6f7.chunk.js"
+  },
+  {
+    "revision": "004c8ddb448ab3a8a4574e6f18483e3a",
+    "url": "/static/js/2.ade9e6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94101585ba5231333965",
+    "url": "/static/js/main.07549502.chunk.js"
+  },
+  {
+    "revision": "9cfbf44c5934bfb6d9ef",
+    "url": "/static/js/runtime-main.b080c05f.js"
+  }
+]);
