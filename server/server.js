9c3ff346d6c6fb4ec1463dfcaac1ea414ed360f9
root <root@ip-172-31-45-134.us-east-2.compute.internal>
2020-03-04 00:05:49 +0000

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const path = require("path");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,28 +12,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+
 
 /**
  * URI to connect to Atlas Database
  * usedNewUrlParser connection for a flag
  * useCreateIndex connection for a flag deprication
  */
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb://localhost/weatherdb",{ useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
-require("./config/routes")(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  if(req.url.match(/.+\/static/)){
-      var url = req.url.match(/\/static.*/);
-      res.redirect(url[0]);
-  }else
-      res.status(404).send('Sorry cant find that!');
-});
+app.use("./config/routes");
+
+
+app.use(express.static( '../build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../build/index.html')); // relative path
+    });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
